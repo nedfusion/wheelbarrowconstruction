56366362e2f9d0859977a11ada69392323c1d301
nedfusion <32440074+nedfusion@users.noreply.github.com>
2025-09-11 14:38:03 +0100

    Increase logo to h-24 dimensions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
             <img
               src="/All caps Transparent.png"
               alt="Wheelbarrow Construction Limited"
-              className="h-16 w-auto"
+             className="h-24 w-auto"
             />
           </div>
           <nav className="hidden lg:flex space-x-10">
